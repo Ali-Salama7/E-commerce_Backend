5e0c89386946e39b37b3ce6a4c3475c6cbbd0e31
Ali Salama <alisalamadev@gmail.com>
2026-07-31 19:29:49 +0300

    feat: initialize prisma with postgres connection

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from 'express'
+import type { Request, Response } from 'express'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello")
+})
 
 app.listen( PORT ,() => {
     console.log(`Server Running on ${PORT}`)
